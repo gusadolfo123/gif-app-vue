34c2c782c38ac13ba9465812ddf07245ed0feee5
gusadolfo123 <gusadolfo123@hotmail.com>
2021-04-07 20:45:03 -0500

    ajuste publish

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,5 +1,5 @@
 const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(
+  const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(
     category
   )}&api_key=rIi9ma7bAf1k6IBrO2AyxS4UpXyAf07F`;
   const resp = await fetch(url);
